1ef79199799d148d9f845ec4c87063946509546d
paulo.santosDell <afonso.mea@gmail.com>
2022-05-17 19:38:25 -0300

    Cria UI - Apresenta resultado, botão de Adicionar e Remover

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Mobx Flutter"),
+        title: const Text("Mobx Flutter", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20 )),
         centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Center(
+            child: Text("000", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.cyan,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text("Adiciona", style: TextStyle(color: Colors.black),),
+                  ),
+                ),
+                SizedBox(width: 40,),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.cyan,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text("Remove", style: TextStyle(color: Colors.black),),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
